17d4bc2c5565e854565a3f2c43f32dfa2c048720
Nicoly Sampaio <78435150+nicolysampaio@users.noreply.github.com>
2024-12-25 22:27:56 -0300

    feat: add author management functionality and integrate with book creation

---
+++ src/routes/authorRoutes.js
@@ -0,0 +1,12 @@
+import express from "express";
+import AuthorController from '../controller/authorController.js';
+
+const routes = express.Router();
+
+routes.get("/authors", AuthorController.getAllAuthors);
+routes.get("/authors/:id", AuthorController.getAuthorById);
+routes.post("/authors", AuthorController.createAuthor);
+routes.put("/authors/:id", AuthorController.updateAuthor);
+routes.delete("/authors/:id", AuthorController.deleteAuthor);
+
+export default routes;
