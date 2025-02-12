159846743c807aac74237ce95efb13dba3396f0a
Nicoly Sampaio <nicoly.sampaioluz@gmail.com>
2025-02-12 17:27:08 -0300

    feat: enhance author controller with improved error handling and response formatting

--- src/controller/authorController.js
+++ src/controller/authorController.js
@@ -1,9 +1,10 @@
+import mongoose from "mongoose";
 import { Author } from "../models/Author.js";
 
 class AuthorController {
   static async getAllAuthors(req, res) {
     try {
-      const authors = await Author.find({});
+      const authors = await Author.find();
 
       res.status(200).json(authors);
     } catch (error) {
@@ -16,22 +17,32 @@ class AuthorController {
 
   static async getAuthorById(req, res) {
     try {
-      const author = await Author.findById(req.params.id);
+      const id = req.params.id;
+      const author = await Author.findById(id);
 
-      res.status(200).json(author);
+      if (author !== null) {
+        res.status(200).send(author);
+      } else {
+        res.status(404).send({ message: "ID not found" });
+      }
     } catch (error) {
-      res.status(500).json({
-        message: "An error occurred while fetching the author.",
-        error: error.message,
-      });
+      if (error instanceof mongoose.Error.CastError) {
+        res.status(400).send({ message: "ID format incorrect" });
+      } else {
+        res.status(500).send({
+          message: "An error occurred while fetching the author.",
+          error: error.message,
+        });
+      }
     }
   }
 
   static async createAuthor(req, res) {
     try {
-      const author = await Author.create(req.body);
+      let author = new Author(req.body);
+      const authorResult = await author.save();
 
-      res.status(201).json({ message: "Author created with success!", author });
+      res.status(201).send(authorResult.toJSON());
     } catch (error) {
       res.status(500).json({
         message: "An error occurred while creating the author.",
@@ -42,11 +53,12 @@ class AuthorController {
 
   static async updateAuthor(req, res) {
     try {
-      await Author.findByIdAndUpdate(req.params.id, req.body);
+      const id = req.params.id;
+      await Author.findByIdAndUpdate(id, { $set: req.body });
 
-      res.status(201).json({ message: "Author updated with success!" });
+      res.status(201).send({ message: "Author updated with success!" });
     } catch (error) {
-      res.status(500).json({
+      res.status(500).send({
         message: "An error occurred while updating the author.",
         error: error.message,
       });
@@ -55,11 +67,12 @@ class AuthorController {
 
   static async deleteAuthor(req, res) {
     try {
-      await Author.findByIdAndDelete(req.params.id);
+      const id = req.params.id;
+      await Author.findByIdAndDelete(id);
 
-      res.status(201).json({ message: "Author deleted with success!" });
+      res.status(201).send({ message: "Author deleted with success!" });
     } catch (error) {
-      res.status(500).json({
+      res.status(500).send({
         message: "An error occurred while deleting the author.",
         error: error.message,
       });
